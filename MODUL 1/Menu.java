8ec988102525602fec771bc8824aba449aeef319
ammarrazan <arazan83@gmail.com>
2022-11-05 08:05:10 +0700

    modul 1

---
+++ MODUL 1/Menu.java
@@ -0,0 +1,13 @@
+public class Menu {
+    public int nama;
+    public int kategori
+    public int harga;
+
+    System.out.println(x,"Nama");
+    System.out.println(x,"Kategori");
+    System.out.println(x,"Harga");
+
+
+    // TODO Create Attribute of Menu; Name, Category, and Price then Create Setter
+
+}
---
+++ MODUL 1/User.java
@@ -0,0 +1,20 @@
+public class User {
+    String nama;
+    String nomer;
+    
+    public void setNama (String nama){
+        this.nama = nama;
+        System.out.println(x,"Nama");
+    }
+    public void setNomer (String nomer){
+        this.nomer = nomer;
+        System.out.println(x,"nomer");
+    }
+
+
+    // TODO Create Attribute of User; Name and Phone Number then Create Setter
+
+    // TODO Create Method to Register User and Display User's Name and Phone Number and success message
+
+
+}
